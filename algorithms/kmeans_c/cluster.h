bb9b7a3ee5efe807b00c88a9e79303deb89e3e6d
Mehul Warade <mehulwarade@gmail.com>
2020-11-29 16:32:36 +1100

    Migrating for workflows

--- algorithms/kmeans_c/cluster.h
+++
@@ -1,1 +0,0 @@
-extern void cluster(data_struct *data_in, data_struct *clusters, int max_iterations);
--- algorithms/kmeans_c/orginal/kmeans.h
+++
@@ -1,7 +0,0 @@
-typedef struct {
-  double *dataset;
-  unsigned int *members;
-  int leading_dim;
-  int secondary_dim; 
-} data_struct;
-
