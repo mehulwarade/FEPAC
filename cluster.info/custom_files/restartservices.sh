123ec387d69cea80bdfa696fed8577885625d3a3
mehul <mehulwarade@gmail.com>
2023-08-15 20:26:48 +1000

    main cleanup

--- cluster.info/custom_files/restartservices.sh
+++
@@ -1,8 +0,0 @@
-#
-#~/restartservices.sh
-#
-#!/bin/sh
-sudo systemctl restart dnsmasq.service
-sudo systemctl restart dnsmasq
-sudo systemctl restart rpcbind
-sudo systemctl restart nfs-kernel-server
