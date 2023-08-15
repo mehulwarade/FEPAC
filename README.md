8f5339ae1fe30831ab49bc2cec8e51199e07e5f8
mehul <mehulwarade@gmail.com>
2023-08-15 20:31:28 +1000

    copy of phd fepac

---
+++ README.md
@@ -0,0 +1,41 @@
+# FEPAC-PHD
+
+This code includes the FEPAC based on 3 switches used during my PHD.
+
+This one just gets energy data from the switches and saves it into the local mysql database.
+
+## Installation
+
+Installing dependencies for the nodeJS.
+Tested connection on 'NETGEAR ProSAFE GS110TP switches'
+
+```
+npm install
+```
+
+### MySQL installation
+
+Need MySQL server installed on local machine
+
+# Running from scratch (Debian 10):
+### RPi netboot. (location: /nfs/client/home/pi/** <= so that its accessible to other nodes =>)
+`sudo apt-get update`
+
+`sudo apt install npm`
+
+<!-- Only if needed -->
+`sudo apt install nodejs`
+    `=> node -v`
+
+`sudo apt install mariadb-server`
+    `=> sudo mysql_secure_installation`
+
+
+# References:
+
+* Clearing cache (cleans all data from RAM => SLOW): https://unix.stackexchange.com/a/87909 
+* MySQL Cheat Sheet: https://gist.github.com/hofmannsven/9164408
+* Max file watch error (in nodemon or VSCode): https://stackoverflow.com/a/34664097 and add ignore params to `nodemonConfig` in package.json
+* Markdown basics: https://wordpress.com/support/markdown-quick-reference/
+* 
+
---
+++ index.js
@@ -0,0 +1,74 @@
+// Await learn https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+// const editJsonFile = require("edit-json-file");
+
+const functions = require("./lib/methods");
+const inquirer = require("./lib/inquirer");
+const mysql = require("./lib/mysql");
+const snmp = require("./lib/snmp");
+const athom = require("./lib/athomPlugs");
+
+var config_path =
+  functions.getConfigPath(); /* Always put this after declaring functions */
+
+clear();
+
+console.log(
+  chalk.yellow(figlet.textSync("FEPAC 2.0", { horizontalLayout: "full" }))
+);
+
+/* Checking for configuration file */
+
+if (functions.directoryExists(config_path)) {
+  var global_env = require(config_path);
+} else {
+  console.log(
+    chalk.red("Configuration file does not exists at " + config_path)
+  );
+  process.exit();
+}
+
+const run = async () => {
+  try {
+    const ask_main = await inquirer.ask_main();
+
+    if (ask_main.res == "Test MySQL connection") {
+      mysql.db_connect((res) => {
+        run();
+      });
+    } else if (ask_main.res == "Test SNMP connection") {
+      snmp.snmp_connect((res) => {
+        run();
+      });
+    } else if (ask_main.res == "MySQL/SNMP connection") {
+      await mysql.snmp((res) => {
+        run();
+      });
+      console.log(chalk.blue(`TABLE [test]: Data being inserted.`));
+      run();
+    } else if (ask_main.res == "Test Athom Plug Connections") {
+      await athom.testConnection((res) => {
+        res ? run() : process.exit;
+      });
+    } else if (ask_main.res == "Athom Plugs mysql insert") {
+      await mysql.athomMySQL((res) => {
+        run();
+      });
+    } else if (ask_main.res == "Stop mysql insert") {
+      await mysql.end();
+      console.log(chalk.blue(`Data insertion halted. Exiting!`));
+      process.exit();
+    } else if (ask_main.res == "Exit") {
+      console.log(chalk.green("Thank you for using FEPAC"));
+      process.exit();
+    }
+  } catch (err) {
+    console.log("errrorrrroro");
+    console.log(chalk.red(err));
+  }
+};
+
+run();
